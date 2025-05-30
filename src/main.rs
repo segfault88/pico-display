d25cb76dac378563476d4230c4185701f788d420
Malcolm Lockyer <segfault88@users.noreply.github.com>
2025-05-31 10:52:37 +1200

    wip

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use embedded_hal::digital::v2::OutputPin;
 use embedded_time::fixed_point::FixedPoint;
 use panic_halt as _;
 
-// Board Support Package for both Pico and Pico 2
+// Board Support Package for Raspberry Pi Pico
 use rp_pico as bsp;
 
 use bsp::hal::{
@@ -26,7 +26,7 @@ fn main() -> ! {
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
+    // External high-speed crystal on the Pico board is 12MHz
     let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
         external_xtal_freq_hz,
@@ -49,7 +49,7 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // LED is on GPIO 25 for both Pico and Pico 2
+    // LED is on GPIO 25 on the Raspberry Pi Pico
     let mut led_pin = pins.led.into_push_pull_output();
 
     loop {
